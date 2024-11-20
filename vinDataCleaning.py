20199e7f88ce38d2274ebd34987448316879373c
alae <alae.ennasyry@gmail.com>
2024-11-20 09:40:05 +0100

    traitement de données concernant le vin

--- vinDataCleaning.py
+++ vinDataCleaning.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import IsolationForest
 
 #lire fichier contenant les données
 uploaded_file = "data/vin.csv"
@@ -19,9 +20,30 @@ scaled_df = pd.DataFrame(scaled_data, columns = df_ex.columns)
 #Encoder la target avec labelEncoder
 label_encoder = LabelEncoder()
 y_encoded = label_encoder.fit_transform(df.target).astype(int)
-y_encoded = pd.DataFrame(y_encoded)
+y_encoded = pd.DataFrame(y_encoded, columns = ['target'])
 
-#Concatener X et y
+#Concatener scaled_df et y_encoded
 clean_df = pd.concat([scaled_df,y_encoded], axis =1)
 
+#pour chaque colonne appliquer isolation forest pour éliminer les valeurs extremes
+for e in scaled_df.columns:
+    #generer un dataframe avec une colonne de zeros
+    y = pd.DataFrame({'zeros': [0.0] * clean_df.shape[0]})
+    
+    #construir un vecteur de deux colonnes pour l'entrée du modèle isolation forest
+    X = pd.concat([y,clean_df[e]], axis =1)
+    
+    #Entrainer un modèle isolation forest
+    model = IsolationForest(contamination=0.02)
+    model.fit(X)
+
+    #utiliser le modèle pour prédire les valeurs extremes
+    X_pred=model.predict(X)
+    
+    #filtrer notre clean_df en utilisant la sortie du modèle isolation forest
+    clean_df = clean_df[X_pred == 1]
+    #redémarer l'index
+    clean_df = clean_df.reset_index(drop = True)
+
+#Exporter le résultat dans un fichier csv
 clean_df.to_csv('cleanVinData.csv', sep=';', index=False)
